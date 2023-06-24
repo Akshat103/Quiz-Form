ed9fc015417e4ffb78f58d554b9f60479dfb2404
Akshat103 <akshattamrakar103@gmail.com>
2023-06-24 13:23:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('form', { score: null });
+});
+
+app.post('/submit', (req, res) => {
+  // Server-side scoring logic
+  const correctAnswers = {
+    question1: 'option2',
+    question2: 'option1',
+    question3: 'option4'
+  };
+
+  let score = 0;
+  for (const question in correctAnswers) {
+    if (req.body[question] === correctAnswers[question]) {
+      score++;
+    }
+  }
+
+  res.render('form',{score})
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
