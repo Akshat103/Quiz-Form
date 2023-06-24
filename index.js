8e3e8ba1979ba76c9fd0f2575f473be101d370b1
Akshat103 <akshattamrakar103@gmail.com>
2023-06-24 15:16:36 +0530

    Modified path

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', (req, res) => {
   res.render('index', { score: null });
